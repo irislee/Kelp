6caa66ff49f4d23d7bf8ec026ad56bf908c2f31a
irislee <irislee513@gmail.com>
2013-11-18 14:40:20 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CityKid::Application.config.secret_key_base = '2d4459011aa646d9f6bb7c9a2f5760ed92785287acb471d0d56c3af9f5526668cd7e49e52e419143d5fc43aca89c3c568f20c636ff7f6165ebe15dabd6ccd82e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CityKid::Application.config.session_store :cookie_store, key: '_city-kid_session'
