3b1be43d06c5e2f5bd2e077b36d54513b92e32d2
irislee <irislee513@gmail.com>
2013-11-26 17:03:07 -0500

    logs in new users automatically

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,10 +25,11 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
+      
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        session[:user_id] = @user.id
+        format.html { redirect_to activities_path, notice: 'User was successfully created.' }
         format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
