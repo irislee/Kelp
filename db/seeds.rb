f8943aa997bf14507c7a69055194619944b664c0
github username <github email address>
2013-12-04 11:59:07 -0500

    added some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,18 +42,72 @@ end
 User.create(:name => "Margaret", :password => "coffee", :password_confirmation => "coffee" )
 User.create(:name => "Iris", :password => "human", :password_confirmation => "human")
 User.create(:name => "Scott", :password => "cheer", :password_confirmation => "cheer")
+User.create(:name => "Avi", :password => "chai", :password_confirmation => "chai" )
+User.create(:name => "Spencer", :password => "magic", :password_confirmation => "magic")
+User.create(:name => "Jonathan", :password => "pigeon", :password_confirmation => "pigeon")
+
+#REVIEW 1
 Activity.find_by(:id => 1).reviews.create(:user_id => 1, :review_text => "This is great! I love taking my kids here.", :activity_id => 1)
 Activity.find_by(:id => 1).ratings.create(:user_id => 1, :score => 4, :activity_id => 1)
-Review.find_by(:id => 1).votes.create(votable_id: 1, votable_type: "Review", voter_id: 2, voter_type: "User", vote_flag: true)
+Review.find_by(:id => 1).votes.create(votable_id: 1, votable_type: "Review", voter_id: 6, voter_type: "User", vote_flag: true)
 Review.find_by(:id => 1).votes.create(votable_id: 1, votable_type: "Review", voter_id: 3, voter_type: "User", vote_flag: false)
 
+#REVIEW 2
 Activity.find_by(:id => 1).reviews.create(:user_id => 3, :review_text => "Picked up the new Saige doll and 'The Smart Girl's Guide to Boys'!", :activity_id => 1)
 Activity.find_by(:id => 1).ratings.create(:user_id => 3, :score => 5, :activity_id => 1)
 Review.find_by(:id => 2).votes.create(votable_id: 1, votable_type: "Review", voter_id: 1, voter_type: "User", vote_flag: true)
-Review.find_by(:id => 2).votes.create(votable_id: 1, votable_type: "Review", voter_id: 2, voter_type: "User", vote_flag: true)
+Review.find_by(:id => 2).votes.create(votable_id: 1, votable_type: "Review", voter_id: 4, voter_type: "User", vote_flag: true)
 
-Activity.find_by(:id => 2).reviews.create(:user_id => 2, :review_text => "Had a really fun time here with my niece on Sunday.", :activity_id => 2)
-Activity.find_by(:id => 2).ratings.create(:user_id => 2, :score => 3, :activity_id => 1)
+#REVIEW 3
+Activity.find_by(:id => 2).reviews.create(:user_id => 4, :review_text => "Had a really fun time here with my niece on Sunday.", :activity_id => 2)
+Activity.find_by(:id => 2).ratings.create(:user_id => 4, :score => 4, :activity_id => 1)
 Review.find_by(:id => 3).votes.create(votable_id: 1, votable_type: "Review", voter_id: 1, voter_type: "User", vote_flag: true)
-Review.find_by(:id => 3).votes.create(votable_id: 1, votable_type: "Review", voter_id: 2, voter_type: "User", vote_flag: true)
+Review.find_by(:id => 3).votes.create(votable_id: 1, votable_type: "Review", voter_id: 3, voter_type: "User", vote_flag: false)
 
+#REVIEW 4
+Activity.find_by(:id => 3).reviews.create(:user_id => 4, :review_text => "Meh-not so great", :activity_id => 3)
+Activity.find_by(:id => 3).ratings.create(:user_id => 4, :score => 2, :activity_id => 3)
+Review.find_by(:id => 4).votes.create(votable_id: 1, votable_type: "Review", voter_id: 5, voter_type: "User", vote_flag: true)
+Review.find_by(:id => 4).votes.create(votable_id: 1, votable_type: "Review", voter_id: 6, voter_type: "User", vote_flag: false)
+
+#REVIEW 5
+Activity.find_by(:id => 4).reviews.create(:user_id => 5, :review_text => "Awesome. 5 Stars. I'm a kid again!", :activity_id => 4)
+Activity.find_by(:id => 4).ratings.create(:user_id => 5, :score => 5, :activity_id => 4)
+Review.find_by(:id => 5).votes.create(votable_id: 1, votable_type: "Review", voter_id: 1, voter_type: "User", vote_flag: true)
+Review.find_by(:id => 5).votes.create(votable_id: 1, votable_type: "Review", voter_id: 3, voter_type: "User", vote_flag: false)
+Review.find_by(:id => 5).votes.create(votable_id: 1, votable_type: "Review", voter_id: 6, voter_type: "User", vote_flag: true)
+
+#REVIEW 6
+Activity.find_by(:id => 5).reviews.create(:user_id => 3, :review_text => "I love this place.  Thanks mom!!!!", :activity_id => 5)
+Activity.find_by(:id => 5).ratings.create(:user_id => 3, :score => 5, :activity_id => 5)
+Review.find_by(:id => 6).votes.create(votable_id: 1, votable_type: "Review", voter_id: 1, voter_type: "User", vote_flag: false)
+Review.find_by(:id => 6).votes.create(votable_id: 1, votable_type: "Review", voter_id: 2, voter_type: "User", vote_flag: false)
+Review.find_by(:id => 6).votes.create(votable_id: 1, votable_type: "Review", voter_id: 4, voter_type: "User", vote_flag: false)
+Review.find_by(:id => 6).votes.create(votable_id: 1, votable_type: "Review", voter_id: 5, voter_type: "User", vote_flag: false)
+
+#REVIEW 7
+Activity.find_by(:id => 6).reviews.create(:user_id => 1, :review_text => "I got sick here. It smells.", :activity_id => 6)
+Activity.find_by(:id => 6).ratings.create(:user_id => 1, :score => 1, :activity_id => 6)
+Review.find_by(:id => 7).votes.create(votable_id: 1, votable_type: "Review", voter_id: 1, voter_type: "User", vote_flag: true)
+Review.find_by(:id => 7).votes.create(votable_id: 1, votable_type: "Review", voter_id: 3, voter_type: "User", vote_flag: true)
+Review.find_by(:id => 7).votes.create(votable_id: 1, votable_type: "Review", voter_id: 4, voter_type: "User", vote_flag: true)
+Review.find_by(:id => 7).votes.create(votable_id: 1, votable_type: "Review", voter_id: 5, voter_type: "User", vote_flag: true)
+
+#REVIEW 8
+Activity.find_by(:id => 7).reviews.create(:user_id => 6, :review_text => "Very clean bathrooms.  I would go again.", :activity_id => 7)
+Activity.find_by(:id => 7).ratings.create(:user_id => 6, :score => 4, :activity_id => 7)
+Review.find_by(:id => 8).votes.create(votable_id: 1, votable_type: "Review", voter_id: 1, voter_type: "User", vote_flag: true)
+Review.find_by(:id => 8).votes.create(votable_id: 1, votable_type: "Review", voter_id: 3, voter_type: "User", vote_flag: true)
+Review.find_by(:id => 8).votes.create(votable_id: 1, votable_type: "Review", voter_id: 5, voter_type: "User", vote_flag: true)
+
+#REVIEW 9
+Activity.find_by(:id => 8).reviews.create(:user_id => 3, :review_text => "The other kids loved my bits!", :activity_id => 8)
+Activity.find_by(:id => 8).ratings.create(:user_id => 3, :score => 5, :activity_id => 8)
+Review.find_by(:id => 8).votes.create(votable_id: 1, votable_type: "Review", voter_id: 1, voter_type: "User", vote_flag: true)
+Review.find_by(:id => 8).votes.create(votable_id: 1, votable_type: "Review", voter_id: 6, voter_type: "User", vote_flag: true)
+
+#REVIEW 10
+Activity.find_by(:id => 9).reviews.create(:user_id => 4, :review_text => "This is the most fun I've had in a week!", :activity_id => 9)
+Activity.find_by(:id => 9).ratings.create(:user_id => 4, :score => 5, :activity_id => 9)
+Review.find_by(:id => 10).votes.create(votable_id: 1, votable_type: "Review", voter_id: 3, voter_type: "User", vote_flag: true)
+Review.find_by(:id => 10).votes.create(votable_id: 1, votable_type: "Review", voter_id: 5, voter_type: "User", vote_flag: true)
